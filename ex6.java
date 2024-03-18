567d9fa4e78c365f7ce0500fb1d12e5e1db11f2a
juliafeitosa <juliafernanda8705@gmail.com>
2024-03-18 20:00:27 -0300

    exercicio 6

---
+++ ex6.java
@@ -0,0 +1,35 @@
+//6. Ler uma temperatura em graus Celsius e apresentá-la convertida em grausFahrenheit. A fórmula de conversão é://
+//F = (9 * C + 160) / 5
+
+
+import java.util.Scanner;
+
+
+
+
+public class ex6 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+
+
+
+        System.out.print("Digite a temperatura em graus Celsius: ");
+        double celsius = scanner.nextDouble();
+
+
+
+
+        double fahrenheit = (9 * celsius + 160) / 5;
+
+
+
+
+        System.out.println("A temperatura em graus Fahrenheit é: " + fahrenheit);
+
+
+
+
+        scanner.close();
+    }
+}
