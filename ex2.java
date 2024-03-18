e5584011c76311fb1c36be63f74b3759565dabbc
juliafeitosa <juliafernanda8705@gmail.com>
2024-03-18 19:51:18 -0300

    exercicio 2

---
+++ ex2.java
@@ -0,0 +1,36 @@
+//Escreva um algoritmo que leia dois números digitados pelo usuário e exiba oresultado da sua soma
+
+
+import java.util.Scanner;
+
+
+public class ex2 {
+
+
+    public static void main(String[] args) {
+        Scanner num = new Scanner(System.in);
+
+
+            System.out.println("infrome o primeiro numero: ");
+            int num1 = num.nextInt();
+
+
+            System.out.println("infrome o segundo numero: ");
+            int num2 = num.nextInt();
+
+
+            int soma = num1 + num2;
+
+
+            System.out.println("o resultado da soma é: " + soma);
+
+
+
+
+
+
+            num.close();
+
+
+    }
+    }
