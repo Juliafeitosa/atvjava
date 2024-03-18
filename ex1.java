e67934d4d3432c409d81313f215ca0465e1c626f
juliafeitosa <juliafernanda8705@gmail.com>
2024-03-18 19:43:54 -0300

    corrigindo

---
+++ ex1.java
@@ -0,0 +1,32 @@
+//Escreva um algoritmo que leia um número digitado pelo usuário e mostre amensagem “Número maior do que 10!”, caso este número seja maior, ou “Númeromenor ou igual a 10!”, caso este número seja menor ou igual
+
+
+import java.util.Scanner;
+
+
+public class ex1 {
+
+
+    public static void main(String[] args) {
+        Scanner num = new Scanner(System.in);
+
+
+        System.out.println("informe o numero: ");
+        int num1 = num.nextInt();
+
+
+        if (num1 > 10) {
+            System.out.println("Número maior do que 10!");
+
+
+        } else {
+            System.out.println("Número menor ou igual a 10!");
+
+
+            num.close();
+
+
+        }
+    }
+}
+
