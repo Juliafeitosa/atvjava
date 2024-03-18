5a70707166a42c7c5d16c5dcd69bae269792380d
juliafeitosa <juliafernanda8705@gmail.com>
2024-03-18 20:30:33 -0300

    exercico 11

---
+++ Lista 1/ex11.java
@@ -0,0 +1,33 @@
+//11. Escreva um algoritmo que leia três valores inteiros distintos e escreva-os emordem crescente
+
+
+import java.util.Scanner;
+
+
+public class ex11 {
+    public static void main(String[] args) {
+
+
+        Scanner scanner = new Scanner(System.in);
+
+
+        System.out.print("Digite o primeiro número: ");
+        int Num1 = scanner.nextInt();
+
+
+        System.out.print("Digite o segundo número: ");
+        int Num2 = scanner.nextInt();
+
+
+        System.out.print("Digite o terceiro número: ");
+        int Num3 = scanner.nextInt();
+
+
+        int menor = Math.min(Math.min(Num1, Num2), Num3);
+        int medio = (Num1 + Num2 + Num3) - menor - Math.max(Math.max(Num1, Num2), Num3);
+        int maior = Math.max(Math.max(Num1, Num2), Num3);
+
+
+        System.out.println("Em ordem crescente: " + menor + ", " + medio + ", " + maior);
+    }
+}
