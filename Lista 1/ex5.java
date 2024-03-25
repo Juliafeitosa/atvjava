ae6a7cee4c62a6aae51056b057ab518dfa6da520
Juliafeitosa <162602756+Juliafeitosa@users.noreply.github.com>
2024-03-25 19:46:07 -0300

    Update ex5.java

--- Lista 1/ex5.java
+++ Lista 1/ex5.java
@@ -8,7 +8,7 @@ import java.util.Scanner;
 
 public class Ex5 {
 
-    public static void variavel (String[] args) {
+    public static void main (String[] args) {
         Scanner num = new Scanner(System.in);
 
        
