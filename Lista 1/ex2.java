243b70bf090323d5e4abff88309e9074c8b78562
Juliafeitosa <162602756+Juliafeitosa@users.noreply.github.com>
2024-03-25 19:45:19 -0300

    Update ex2.java

--- Lista 1/ex2.java
+++ Lista 1/ex2.java
@@ -7,7 +7,7 @@ import java.util.Scanner;
 public class Ex2 {
 
 
-    public static void somar (String[] args) {
+    public static void main (String[] args) {
         Scanner num = new Scanner(System.in);
 
 
