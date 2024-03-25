fad649a889b143396a17946cc06ae402037929e7
Juliafeitosa <162602756+Juliafeitosa@users.noreply.github.com>
2024-03-25 19:43:15 -0300

    Update ex13.java

--- Lista 1/ex13.java
+++ Lista 1/ex13.java
@@ -6,7 +6,7 @@ import java.util.Scanner;
 
 
 public class Ex13 {
-    public static void case(String[] args) {
+    public static void main(String[] args) {
 
 
         Scanner scanner = new Scanner(System.in);
