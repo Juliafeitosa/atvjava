9726f5121c10e087b0727044bb33e16d6d8c17c0
Juliafeitosa <162602756+Juliafeitosa@users.noreply.github.com>
2024-03-25 09:26:38 -0300

    Update ex8.java

--- Lista 1/ex8.java
+++ Lista 1/ex8.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 
 
 public class ex8 {
-    public static void main(String[] args) {
+    public static void numeros (String[] args) {
         Scanner scanner = new Scanner(System.in);
 
 
