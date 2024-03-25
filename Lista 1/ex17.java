c9c9dc21e109f793fcc987c887a6cf013abc4849
Juliafeitosa <162602756+Juliafeitosa@users.noreply.github.com>
2024-03-25 19:44:27 -0300

    Update ex17.java

--- Lista 1/ex17.java
+++ Lista 1/ex17.java
@@ -7,7 +7,7 @@ import java.util.Scanner;
 
 
 public class Ex17 {
-    public static void mediaPonderada(String[] args) {
+    public static void main(String[] args) {
 
 
         Scanner scanner = new Scanner(System.in);
