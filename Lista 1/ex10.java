2d0d7ba823075a8a133fd5f2e0d2ec677462d82d
Juliafeitosa <162602756+Juliafeitosa@users.noreply.github.com>
2024-03-25 19:42:42 -0300

    Update ex10.java

--- Lista 1/ex10.java
+++ Lista 1/ex10.java
@@ -6,7 +6,7 @@ import java.util.Scanner;
 
 
 public class Ex10 {
-    public static void case(String[] args) {
+    public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
 
