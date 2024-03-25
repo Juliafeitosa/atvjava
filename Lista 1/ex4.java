840195ee8c14e9eedf7e093f5aeda2735d4814a5
Juliafeitosa <162602756+Juliafeitosa@users.noreply.github.com>
2024-03-25 08:25:25 -0300

    Update ex4.java

--- Lista 1/ex4.java
+++ Lista 1/ex4.java
@@ -7,7 +7,7 @@ import java.util.Scanner;
 public class ex4 {
 
 
-    public static void main(String[] args) {
+    public static void calculadora (String[] args) {
         Scanner num = new Scanner(System.in);
 
 
