65d0765d8a7597c38d886cf42b17742ba8059cb6
Juliafeitosa <162602756+Juliafeitosa@users.noreply.github.com>
2024-03-25 09:25:42 -0300

    Update ex6.java

--- Lista 1/ex6.java
+++ Lista 1/ex6.java
@@ -8,7 +8,7 @@ import java.util.Scanner;
 
 
 public class ex6 {
-    public static void main(String[] args) {
+    public static void celsius (String[] args) {
         Scanner scanner = new Scanner(System.in);
 
 
