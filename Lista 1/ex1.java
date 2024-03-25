3d0756224794230203bf1fdee2fba7716c8dfa74
Juliafeitosa <162602756+Juliafeitosa@users.noreply.github.com>
2024-03-25 08:24:27 -0300

    Update ex1.java

--- Lista 1/ex1.java
+++ Lista 1/ex1.java
@@ -7,7 +7,7 @@ import java.util.Scanner;
 public class ex1 {
 
 
-    public static void main(String[] args) {
+    public static void numeros (String[] args) {
         Scanner num = new Scanner(System.in);
 
 
