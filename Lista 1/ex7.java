d4589f97c5c809adbc7b10faeee1aaabf9ae1d40
Juliafeitosa <162602756+Juliafeitosa@users.noreply.github.com>
2024-03-25 09:26:09 -0300

    Update ex7.java

--- Lista 1/ex7.java
+++ Lista 1/ex7.java
@@ -6,7 +6,7 @@ import java.util.Scanner;
 
 
 public class ex7 {
-    public static void main(String[] args) {
+    public static void intervalo (String[] args) {
         Scanner scanner = new Scanner(System.in);
 
 
