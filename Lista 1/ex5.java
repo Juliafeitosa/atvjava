05ddfdaca3815d031c0ab47b74270736fbbd2394
Juliafeitosa <162602756+Juliafeitosa@users.noreply.github.com>
2024-03-25 09:25:14 -0300

    Update ex5.java

--- Lista 1/ex5.java
+++ Lista 1/ex5.java
@@ -6,49 +6,27 @@
 
 import java.util.Scanner;
 
-
-
-
 public class ex5 {
 
-
-
-
-    public static void main(String[] args) {
+    public static void variavel (String[] args) {
         Scanner num = new Scanner(System.in);
-
-
-
 
        
         System.out.print("Digite o valor de A: ");
         int A = num.nextInt();
-
-
-
+        
 
         System.out.print("Digite o valor de B: ");
         int B = num.nextInt();
-
-
-
-
-       
+ 
         System.out.println("Valores originais:");
         System.out.println("A = " + A);
         System.out.println("B = " + B);
-
-
-
 
         System.out.println("Valores trocados:");
         System.out.println("A = " + B);
         System.out.println("B = " + A);
 
-
-
-
- 
         num.close();
         }
     }
